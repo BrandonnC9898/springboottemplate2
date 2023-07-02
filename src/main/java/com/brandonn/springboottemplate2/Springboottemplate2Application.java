31711ed1295d2585b8e599f104d6208ea8f662a0
BrandonnC9898 <brandonn9898@gmail.com>
2023-07-01 20:40:18 -0500

    Setup

---
+++ src/main/java/com/brandonn/springboottemplate2/Springboottemplate2Application.java
@@ -0,0 +1,13 @@
+package com.brandonn.springboottemplate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboottemplate2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboottemplate2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/brandonn/springboottemplate2/Springboottemplate2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brandonn.springboottemplate2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboottemplate2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
