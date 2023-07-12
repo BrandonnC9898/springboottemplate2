84a262a5b47a751657e44ef4b8a68ea0ae87b945
BrandonnC9898 <brandonn9898@gmail.com>
2023-07-12 16:46:35 -0500

    Add error response

--- src/main/java/com/brandonn/springboottemplate2/shared/errorhandler/RestErrorHandler.java
+++ src/main/java/com/brandonn/springboottemplate2/shared/errorhandler/RestErrorHandler.java
@@ -1,7 +1,12 @@
 package com.brandonn.springboottemplate2.shared.errorhandler;
 
+import com.brandonn.springboottemplate2.shared.dto.BasicHttpResponseDto;
+import com.brandonn.springboottemplate2.shared.dto.ErrorHttpResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -13,10 +18,23 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Slf4j
 public class RestErrorHandler extends ResponseEntityExceptionHandler {
 
+    @Value("${info.app.version}")
+    private String version;
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
         log.error(ex.getMessage());
-        return ResponseEntity.badRequest().body(ex.getBody());
+        var response = new BasicHttpResponseDto<ErrorHttpResponseDto>();
+        response.setVersion(version);
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setMessage("Error");
+        var error = new ErrorHttpResponseDto();
+        error.setErrorCode(1000);
+        var errors = ex.getBindingResult().getAllErrors().stream()
+                .map(e -> ((DefaultMessageSourceResolvable) e.getArguments()[0]).getDefaultMessage() + " " + e.getDefaultMessage()).toList();
+        error.setDescription(String.valueOf(errors));
+        response.setData(error);
+        return ResponseEntity.badRequest().body(response);
     }
 }
